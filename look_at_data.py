c7231970f930d8ffda70fd639ef53decbfc466a4
mattbellis <matthew.bellis@gmail.com>
2017-07-06 14:38:14 -0400

    Starter code

---
+++ look_at_data.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+import ROOT
+
+import sys
+
+infile = sys.argv[1]
+
+f = ROOT.TFile(infile)
+
+t = f.Get("ana/hgc")
+
+# Definitions here
+# https://github.com/CMS-HGCAL/reco-ntuples/blob/master/Definitions.md
+t.Print()
+
+# Found some of the definitions here
+# CaloCluster
+# http://cmsdoxygen.web.cern.ch/cmsdoxygen/CMSSW_9_2_1/doc/html/d4/d06/classreco_1_1CaloCluster.html
+
+nentries = t.GetEntries()
+
+for i in range(nentries):
+
+    t.GetEntry(i)
+
+    print(i)
+    x = t.multiclus_energy
+
+    print(len(x))
+
+
