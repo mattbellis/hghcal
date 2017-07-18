683325e8c3018de03946cb459362574d56d48a52
Madeline Hagen <me25hage@siena.edu>
2017-07-18 11:31:21 -0500

    Updated look_at_data

--- look_at_data.py
+++ look_at_data.py
@@ -8,8 +8,10 @@ import sys
 infile = sys.argv[1]
 
 f = ROOT.TFile(infile)
-
 t = f.Get("ana/hgc")
+
+#t = f.Get("ana")
+#t = t.Get("hgc")
 
 # Definitions here
 # https://github.com/CMS-HGCAL/reco-ntuples/blob/master/Definitions.md
@@ -20,7 +22,7 @@ t.Print()
 # http://cmsdoxygen.web.cern.ch/cmsdoxygen/CMSSW_9_2_1/doc/html/d4/d06/classreco_1_1CaloCluster.html
 
 nentries = t.GetEntries()
-
+layers = []
 for i in range(nentries):
 
     t.GetEntry(i)
@@ -28,6 +30,15 @@ for i in range(nentries):
     print(i)
     x = t.multiclus_energy
 
-    print(len(x))
+    #print(len(x))
+    
+    layer = t.rechit_layer
+    layers.append(layer)
 
 
+
+
+
+plt.figure()
+plt.hist(layers)
+plt.show()
