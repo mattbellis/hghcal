ab90e2d18cd9e7383fce3d456f659341b72733d7
me25hage <me25hage@siena.edu>
2018-04-02 23:47:19 -0500

    mippt -> energy(mip) conversions added

--- energytoet.py
+++ energytoet.py
@@ -11,9 +11,11 @@ if type(filenames) == str:
 
 energies = []
 etas = []
+thetas = []
 layers = []
 energiesLAYER = []
 mippts = []
+Emips = []
 mipptsLAYER = []
 
 for i in range(50):
@@ -25,9 +27,12 @@ fnum = 0
 for filename in filenames:
     energies.append([])
     etas.append([])
+    thetas.append([])
     mippts.append([])
+    Emips.append([])
     layers.append([])
-
+    
+    
     f = ROOT.TFile(filename)
     #f.cd("hgcalTriggerNtuplizer")
 
@@ -46,10 +51,18 @@ for filename in filenames:
         mippt = t.tc_mipPt
         eta = t.tc_eta
         layer = t.tc_layer
+
+        etatmp = np.array(eta)
+        mippttmp = np.array(mippt)
+        theta = 2 * np.arctan(np.exp(-1*etatmp))
+   
+        Emipstmp = mippttmp / np.sin(theta) 
     
         energies[fnum] += (energy)
         mippts[fnum] += (mippt)
+        Emips[fnum] += (Emipstmp.tolist())
         etas[fnum] += (eta)
+        thetas[fnum] += theta.tolist()
         layers[fnum] += (layer)
     
         
@@ -64,6 +77,7 @@ etas = np.array(etas)[0]
 layers = np.array(layers)[0]
 energies = np.array(energies)[0]
 mippts = np.array(mippts)[0]
+Emips = np.array(Emips)[0]
 
 print(type(etas))
 print(len(etas))
@@ -72,21 +86,25 @@ print(len(etas))
 # Histogram the basic quantities 
 ################################################################################
 plt.figure(figsize=(8,8))
-plt.subplot(2,2,1)
+plt.subplot(2,3,1)
 plt.hist(etas,bins=100)
 plt.xlabel(r"$\eta$",fontsize=18)
 
-plt.subplot(2,2,2)
+plt.subplot(2,3,2)
 plt.hist(layers,bins=60,range=(0,30))
 plt.xlabel("Layer",fontsize=18)
 
-plt.subplot(2,2,3)
+plt.subplot(2,3,3)
 plt.hist(energies,bins=100,range=(0,2))
 plt.xlabel("Energy",fontsize=18)
 
-plt.subplot(2,2,4)
+plt.subplot(2,3,4)
 plt.hist(mippts,bins=100,range=(0,10))
 plt.xlabel("mippt",fontsize=18)
+
+plt.subplot(2,3,5)
+plt.hist(Emips,bins=100,range=(0,10))
+plt.xlabel("Energy (mip)",fontsize=18)
 
 plt.tight_layout()
 
@@ -138,8 +156,9 @@ plt.title('Eta cuts')
 
 for g in range(len(etasmask)):
     plt.subplot(4,4,g+1)
-    plt.xlabel('energy')
-    plt.ylabel('mippt')
+    plt.xlabel('energy(gev)')
+    #plt.ylabel('mippt')
+    plt.ylabel('energy(mip)')
     plt.xlim(x1,x2)
     plt.ylim(y1,y2)
     #x = energies*etasmask[g]
@@ -147,20 +166,23 @@ for g in range(len(etasmask)):
     #plt.plot(x[0],y[0],'.',label='Cut: ' + str(cut))
     print(len(energies),len(etasmask[g]),len(etas))
     x = energies[etasmask[g]]
-    y = mippts[etasmask[g]]
+    #y = mippts[etasmask[g]]
+    y = Emips[etasmask[g]]
     plt.plot(x,y,'.',markersize=2,alpha=0.3,label=str(etacuts[g][0]) + r'< $\eta$ ' + str(etacuts[g][1]))
     print(g)
     plt.legend()
     #cut += .025
 
 plt.figure(figsize=(10,10))
-plt.xlabel('energy')
-plt.ylabel('mippt')
+plt.xlabel('energy(gev)')
+#plt.ylabel('mippt')
+plt.ylabel('energy(mip)')
 cut = 1.475
 
 for i in range(len(etasmask)):
     x = energies[etasmask[i]]
-    y = mippts[etasmask[i]]
+    #y = mippts[etasmask[i]]
+    y = Emips[etasmask[i]]
     plt.plot(x,y,'.',markersize=5,alpha=0.3,label=str(etacuts[i][0]) + r'< $\eta$ ' + str(etacuts[i][1]))
     #cut += .025
 
@@ -172,13 +194,15 @@ plt.legend(markerscale=5)
 
 plt.figure(figsize=(12,8))
 #plt.gcf().subplots_adjust(hspace=0.01, wspace=0.01) 
-plt.xlabel('energy')
-plt.ylabel('mippt')
+plt.xlabel('energy(gev)')
+#plt.ylabel('mippt')
+plt.ylabel('energy(mip)')
 
 for i in range(len(layermask)):
     plt.subplot(3,5,int(i/2)+1)
     x = energies[layermask[i]]
-    y = mippts[layermask[i]]
+    #y = mippts[layermask[i]]
+    y = Emips[layermask[i]]
     plt.plot(x,y,'.',markersize=3,label='Layer: ' + str(i+1))
 
     plt.xlim(x1,x2)
@@ -188,9 +212,9 @@ for i in range(len(layermask)):
 
 
     if i>20:
-        plt.xlabel('energy')
+        plt.xlabel('energy(gev)')
     if i%10==0:
-        plt.ylabel('mippt')
+        plt.ylabel('energy(mip)')
 
 plt.tight_layout()
 plt.subplots_adjust(wspace=0, hspace=0)
@@ -217,10 +241,6 @@ for j in range(len(etasmask)):
         plt.plot(x[0],y[0],'.')
         plt.xlim(x1,x2)
         plt.ylim(y1,y2)
-'''
-plt.ifigure()
-for i in range(len(energiesETA[1])):
-    plt.plot(energiesETA[1][i],mipptsETA[1][i],'.')
-plt.title('Eta < 1.5')
-'''
+
+
 plt.show()
